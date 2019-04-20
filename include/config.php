12edabf785212ba7e4a0013b543a54a6805fe3f2
Kanit Wijitpanya <okikzaken@gmail.com>
2019-04-20 14:30:45 +0700

    g
    g

--- include/config.php
+++ include/config.php
@@ -2,12 +2,12 @@
 $server = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = '';
+$dbname = 'vote2562';
 
 $con = mysqli_connect($server,$username,$password,$dbname);
 
 if($con){
-
+    echo "เชื่อมต่อฐานข้อมูลสำเร็จ";
 }else{
     echo error_log($con);
 }
